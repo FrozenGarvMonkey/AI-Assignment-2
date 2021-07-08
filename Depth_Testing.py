1ce4ecaffeee4af1278b37581d56b85adb7e6490
Garv Nair <32934519+garvnair@users.noreply.github.com>
2021-07-09 01:37:10 +0800

    Final

--- Depth_Testing.py
+++ Depth_Testing.py
@@ -2,8 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.tree import plot_tree
-import random
 
 # Data files
 heart = pd.read_csv("Data/heart.csv")
@@ -29,7 +27,7 @@ ax2.set_ylabel("Test Accuracy")
 fig3, ax3 = plt.subplots(1)
 ax3.set_title("Actual Depth over Max Depth")
 ax3.set_xlim(1, 40)
-ax3.set_ylim(0, 10)
+ax3.set_ylim(0, 20)
 ax3.set_xlabel("Max Depth")
 ax3.set_ylabel("Actual Depth")
 
@@ -37,12 +35,13 @@ markers = ["o", "*", "s", "^"]
 labels = ["Test 1", "Test 2", "Test 3", "Test 4"]
 max_depths = [x for x in range(1, 41)]
 
+
 for marker in markers:
     test_accuracy = []
     train_accuracy = []
     depth_used = []
     train_data, test_data, train_result, test_result = train_test_split(
-        datas, results, test_size=0.2, random_state=None, shuffle=True
+        datas, results, test_size=0.2, random_state=42, shuffle=True
     )
     for depth in max_depths:
         classifier = DecisionTreeClassifier(
@@ -65,7 +64,6 @@ for marker in markers:
 
 ax1.scatter(7, train_accuracy[6], s=200, facecolors="none", edgecolors="r")
 ax3.scatter(7, depth_used[6], s=200, facecolors="none", edgecolors="r")
-
 fig1.savefig("Train Accuracy over Max Depth.png")
 fig2.savefig("Test Accuracy over Max Depth.png")
 fig3.savefig("Actual Depth over Max Depth.png")
