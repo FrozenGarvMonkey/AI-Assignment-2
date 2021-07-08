7699dc8876cf62932d300a6dd128e14a8bd0783a
Garv Nair <32934519+garvnair@users.noreply.github.com>
2021-07-08 17:02:51 +0800

    Working Decision Tree

--- DecisionTree.py
+++ DecisionTree.py
@@ -2,46 +2,43 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
-
+from sklearn.tree import plot_tree
 
 # Data files
-heart = pd.read_csv("Data/heart.csv")
+heart = pd.read_csv("input/heart.csv")
 
 # Splitting Dependant and Independant variables
 results = heart["output"]
 datas = heart.drop(["output", "oldpeak", "slp"], inplace=False, axis=1)
 
 # Initializing Chart
-plt.figure()
-plt.subplot(1, 3, 1)
-plt.title("Depth - Test Accuracy")
-plt.xlim(0, 40)
-plt.ylim(0, 1)
-plt.xlabel("Depth")
-plt.ylabel("Accuracy")
+fig1, ax1 = plt.subplots(1)
+ax1.set_title("Depth comparison")
+ax1.set_xticks([i for i in range(1, 41)])
+ax1.set_xlabel("Depth")
+ax1.set_ylabel("Accuracy")
 
-plt.subplot(1, 3, 2)
-plt.title("Max Depth - Actual Depth")
-plt.xlim(0, 40)
-plt.ylim(0, 40)
-plt.xlabel("Max Depth")
-plt.ylabel("Actual Depth")
+fig2, ax2 = plt.subplots(1)
+ax2.set_title("Depth comparison")
+ax2.set_xticks([i for i in range(1, 41)])
+ax2.set_xlabel("Max Depth")
+ax2.set_ylabel("Actual Depth")
 
-plt.subplot(1, 3, 3)
-plt.title("Depth - Train Accuracy")
-plt.xlim(0, 40)
-plt.ylim(0, 1)
-plt.xlabel("Depth")
-plt.ylabel("Accuracy")
+fig3, ax3 = plt.subplots(1)
+ax3.set_title("Depth - Train Accuracy")
+ax3.set_xlim(0, 40)
+ax3.set_ylim(0, 2)
+ax3.set_xlabel("Depth")
+ax3.set_ylabel("Accuracy")
 
 markers = ["o", "*", ".", "s", "^"]
 
 for marker in markers:
-    test_accuracy = []
+    accuracy = []
     train_accuracy = []
     depth_used = []
     train_data, test_data, train_result, test_result = train_test_split(
-        datas, results, test_size=0.2, random_state=True
+        datas, results, test_size=0.2, random_state=25
     )
     for depth in range(1, 41):
         classifier = DecisionTreeClassifier(
@@ -49,15 +46,18 @@ for marker in markers:
         )
         classifier.fit(train_data, train_result)
         prediction = classifier.predict(test_data)
-        test_accuracy.append(classifier.score(test_data, test_result))
+
+        accuracy.append(classifier.score(test_data, test_result))
         depth_used.append(classifier.get_depth())
+        print(classifier.get_n_leaves())
         train_accuracy.append(classifier.score(train_data, train_result))
 
-    plt.subplot(1, 3, 1)
-    plt.plot(test_accuracy, marker=marker)
-    plt.subplot(1, 3, 2)
-    plt.plot(depth_used, marker=marker)
-    plt.subplot(1, 3, 3)
-    plt.plot(train_accuracy, marker=marker)
+    ax1.plot(accuracy, marker=marker)
+    ax2.plot(depth_used, marker=marker)
+    ax3.plot(train_accuracy, marker=marker)
+
+fig1.savefig("graph1.png")
+fig2.savefig("graph2.png")
+fig3.savefig("graph3.png")
 
 plt.show()
