49450d7a3b6c7a40079b12f2b05a1e0240234797
Garv Nair <32934519+garvnair@users.noreply.github.com>
2021-07-09 12:51:36 +0800

    Final Final

--- Depth_Testing.py
+++ Depth_Testing.py
@@ -2,13 +2,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
+import random
 
-# Data files
+random.seed(42)
+
+# Initializing Data
 heart = pd.read_csv("Data/heart.csv")
-
-# Splitting Dependant and Independant variables
 results = heart["output"]
-datas = heart.drop(["output", "oldpeak", "slp"], inplace=False, axis=1)
+datas = heart.loc[:, ["age", "cp", "trtbps", "chol", "thalachh"]]
 
 # Initializing Chart
 fig1, ax1 = plt.subplots(1)
@@ -31,19 +32,25 @@ ax3.set_ylim(0, 20)
 ax3.set_xlabel("Max Depth")
 ax3.set_ylabel("Actual Depth")
 
+# Initializing Default values
 markers = ["o", "*", "s", "^"]
 labels = ["Test 1", "Test 2", "Test 3", "Test 4"]
 max_depths = [x for x in range(1, 41)]
 
-
+# Different Trial Runs
 for marker in markers:
     test_accuracy = []
     train_accuracy = []
     depth_used = []
+
+    # New Data Split Every Trial Run
     train_data, test_data, train_result, test_result = train_test_split(
-        datas, results, test_size=0.2, random_state=42, shuffle=True
+        datas, results, test_size=0.2, random_state=random.randint(1, 20), shuffle=True
     )
+
+    # Test Different max_depth Values (1-40)
     for depth in max_depths:
+        # New Classifier with Different max_depth Values
         classifier = DecisionTreeClassifier(
             criterion="gini", splitter="best", max_depth=depth
         )
@@ -54,6 +61,7 @@ for marker in markers:
         depth_used.append(classifier.get_depth())
         train_accuracy.append(classifier.score(train_data, train_result))
 
+    # Plotting Trail Run
     ax1.plot(
         max_depths, train_accuracy, marker=marker, label=labels[markers.index(marker)]
     )
@@ -62,8 +70,9 @@ for marker in markers:
     )
     ax3.plot(max_depths, depth_used, marker=marker, label=labels[markers.index(marker)])
 
-ax1.scatter(7, train_accuracy[6], s=200, facecolors="none", edgecolors="r")
-ax3.scatter(7, depth_used[6], s=200, facecolors="none", edgecolors="r")
+# Marking max depth value chosen
+ax1.scatter(9, train_accuracy[8], s=600, facecolors="none", edgecolors="r")
+ax3.scatter(9, depth_used[8], s=600, facecolors="none", edgecolors="r")
 fig1.savefig("Train Accuracy over Max Depth.png")
 fig2.savefig("Test Accuracy over Max Depth.png")
 fig3.savefig("Actual Depth over Max Depth.png")
--- Tree.py
+++ Tree.py
@@ -1,58 +1,62 @@
+from numpy.lib.function_base import average
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.tree import plot_tree
 
-# Data files
+# Initializing Data
 heart = pd.read_csv("Data/heart.csv")
-
-# Splitting Dependant and Independant variables
 results = heart["output"]
-datas = heart.drop(["output", "oldpeak", "slp"], inplace=False, axis=1)
+datas = heart.loc[:, ["age", "cp", "trtbps", "chol", "thalachh"]]
 
-# Classifier
-train_data, test_data, train_result, test_result = train_test_split(
-    datas, results, test_size=0.2, random_state=5, shuffle=True
-)
-classifier = DecisionTreeClassifier(criterion="gini", splitter="best", max_depth=7)
-classifier.fit(train_data, train_result)
-results = classifier.predict(test_data)
-accuracy = classifier.score(test_data, test_result)
-
-# Accuracy plotting
-test_result = test_result.values.tolist()
+# Accurary Recording Variables
+accuracy = []
 t_positive = 0
 t_negative = 0
 f_positive = 0
 f_negative = 0
 
-for i in range(len(results)):
-    if results[i] == 0:
-        if test_result[i] == 0:
-            t_positive += 1
-        else:
-            f_positive += 1
-    else:
-        if test_result[i] == 1:
-            t_negative += 1
-        else:
-            f_negative += 1
+# Split Data Randomly with random_state Seed
+train_data, test_data, train_result, test_result = train_test_split(
+    datas, results, test_size=0.2, random_state=5, shuffle=True
+)
+test_result = test_result.values.tolist()
 
+# 50 Trial runs
+for i in range(50):
+    # New Classifier and Decision Tree Every Run
+    classifier = DecisionTreeClassifier(criterion="gini", splitter="best", max_depth=9)
+    classifier.fit(train_data, train_result)
+
+    # Accuracy Recoding
+    results = classifier.predict(test_data)
+    accuracy.append(classifier.score(test_data, test_result))
+    for i in range(len(results)):
+        if results[i] == 0:
+            if test_result[i] == 0:
+                t_positive += 1
+            else:
+                f_positive += 1
+        else:
+            if test_result[i] == 1:
+                t_negative += 1
+            else:
+                f_negative += 1
+average_acc = sum(accuracy) / len(accuracy)
+
+# Plotting and Visualizing Bar Chart
 plt.figure()
 plt.bar(
     ["True Low Risk", "True High Risk", "False Low Risk", "False High Risk"],
     [t_positive, t_negative, f_positive, f_negative],
 )
-plt.title("Accuracy = {:%}".format(accuracy))
+plt.title("Average Accuracy = {:%}".format(average_acc))
 plt.savefig("Result Accuracy.png", bbox_inches="tight")
 plt.show()
 
-# initialising chart
+# Plotting and Visualizing Last Trial Run's Decision Tree
 plt.figure(figsize=(50, 50))
-
-# Tree Diagram
 plot_tree(classifier, filled=True, rounded=False, fontsize=10)
 plt.savefig("Decision Tree.png", bbox_inches="tight")
-
 plt.show()
